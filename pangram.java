5c3daa334eb4d55a0ddf1d54a0382ff58f10e40f
Saikumar Neerati <127942063+saikumarmudiraj@users.noreply.github.com>
2023-10-26 11:13:15 +0530

    Add files via upload

---
+++ pangram.java
@@ -0,0 +1,27 @@
+public class Pangram
+{
+	public static void main(String [] args)
+	{
+		String s="The quick brown fox jumps over the lazy dog";
+		System.out.println(isPangram(s.toLowerCase()));
+	}
+	private static boolean isPangram(String s) 
+	{
+		if (s.length()<26)
+		{
+			return false;
+		}
+		else
+			{
+			    for (char ch='a';ch<='z' ;ch++ )
+				{
+					if (s.indexOf(ch)<0)
+					{
+					return false;
+					}
+				}
+			
+			return true;
+		}
+	}
+} 
